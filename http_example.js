f0f86667094127d680260ffbc75905e7563a9b47
Andrew Hsieh <andrewhsieh1208@gmail.com>
2018-08-05 19:18:49 +0000

    added response for stream

---
+++ http_example.js
@@ -0,0 +1,19 @@
+var https = require('https');
+
+console.log('I did it!');
+
+var options = {
+  host: 'www.example.org',
+  path: '/'
+};
+
+var callback = function(response) {
+  console.log('In response handler callback!');
+  console.log('response: ', response)
+}
+
+console.log("i'm about to make the request")
+
+https.request(options, callback).end();
+
+console.log("I've made the request!");
